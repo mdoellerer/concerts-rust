06ab8aaa846966234bb89589140cde3dda745fb4
mdoellerer <mdoellerer@gmail.com>
2020-06-05 14:44:03 +0200

    DB connection and Connection Pool added

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,25 @@ use handlers::venues;
 use handlers::concerts;
 use handlers::concert_types;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{dev::ServiceRequest, web, App, Error, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=debug");
 
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // create db connection pool
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+        
     // Start http server
     HttpServer::new(move || {
         App::new()
