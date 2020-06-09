c81aa69e85a1bfa3714c75809ec24f7b5519001f
mdoellerer <mdoellerer@gmail.com>
2020-06-09 17:21:58 +0200

    Basic features for Concert Handler

--- src/handlers/concerts.rs
+++ src/handlers/concerts.rs
@@ -1,22 +1,103 @@
+use super::models::{Concert, NewConcert, InputConcert, UpdateConcert};
+use super::schema::concerts::dsl::*;
+use super::Pool;
+use diesel::QueryDsl;
+use diesel::RunQueryDsl;
+use actix_web::{web, Error, HttpResponse };
+use diesel::dsl::{delete, insert_into};
+use std::vec::Vec;
 
-use actix_web::{Responder};
-
-pub async fn get_concerts() -> impl Responder {
-    format!("GET LIST")
+// Handler for GET /concerts/
+pub async fn get_concerts(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
+    Ok(web::block(move || db_get_all_concerts(db))
+        .await
+        .map(|concert| HttpResponse::Ok().json(concert))
+        .map_err(|_| HttpResponse::InternalServerError())?
+    )
 }
 
-pub async fn add_concert() -> impl Responder {
-    format!("POST ONE")
+fn db_get_all_concerts(db: web::Data<Pool>) -> Result<Vec<Concert>, diesel::result::Error> {
+    let conn = db.get().unwrap();
+    let items = concerts.load::<Concert>(&conn)?;
+    Ok(items)
 }
 
-pub async fn get_concert_by_id() -> impl Responder {
-    format!("GET ONE")
+// Handler for GET /concerts/{id}
+pub async fn get_concert_by_id(db: web::Data<Pool>, concert_id: web::Path<i64>) -> Result<HttpResponse, Error> {
+    Ok(web::block(move || db_get_concert_by_id(db, concert_id.into_inner()))
+        .await
+        .map(|concert| HttpResponse::Ok().json(concert))
+        .map_err(|_| HttpResponse::InternalServerError())?
+    )
 }
 
-pub async fn edit_concert() -> impl Responder {
-    format!("EDIT ONE")
+fn db_get_concert_by_id(db: web::Data<Pool>, concert_id: i64) -> Result<Concert, diesel::result::Error> {
+    let conn = db.get().unwrap();
+    concerts.find(concert_id).get_result::<Concert>(&conn)    
 }
 
-pub async fn delete_concert() -> impl Responder {
-    format!("DELETE ONE")
+// Handler for POST /concerts/
+pub async fn add_concert(db: web::Data<Pool>, item: web::Json<InputConcert>) -> Result<HttpResponse, Error> {
+    Ok(web::block(move || db_add_single_concert(db, item))
+        .await
+        .map(|concert| HttpResponse::Ok().json(concert))
+        .map_err(|_| HttpResponse::InternalServerError())?
+    )
+}
+
+fn db_add_single_concert(db: web::Data<Pool>, item: web::Json<InputConcert>) -> Result<Concert, diesel::result::Error> {
+    let conn = db.get().unwrap();
+    let concert_date_value = chrono::NaiveDate::parse_from_str(&item.concert_date, "%Y-%m-%d").unwrap();
+    let new_concert = NewConcert {
+        concert_date: concert_date_value,
+        setlist: &item.setlist,
+        created_at: chrono::Local::now().naive_local(),
+        updated_at: chrono::Local::now().naive_local(),
+        concert_type_id: item.concert_type_id,
+        artist_id: item.artist_id,
+        venue_id: item.venue_id,
+    };
+    let res = insert_into(concerts).values(&new_concert).get_result(&conn)?;
+    Ok(res)
+}
+
+// Handler for PATCH /concerts/{id}
+pub async fn edit_concert(db: web::Data<Pool>, concert_id: web::Path<i64>, item: web::Json<InputConcert>) -> Result<HttpResponse, Error> {
+    Ok(web::block(move || db_edit_single_concert(db, concert_id.into_inner(), item))
+        .await
+        .map(|concert| HttpResponse::Created().json(concert))
+        .map_err(|_| HttpResponse::InternalServerError())?)
+}
+
+fn db_edit_single_concert(db: web::Data<Pool>, concert_id: i64, item: web::Json<InputConcert>) -> Result<Concert, diesel::result::Error> {
+    let conn = db.get().unwrap();
+    let concert = concerts.find(concert_id).get_result::<Concert>(&conn)?;
+    let concert_date_value = chrono::NaiveDate::parse_from_str(&item.concert_date, "%Y-%m-%d").unwrap();
+    let update = UpdateConcert {
+        concert_date: concert_date_value,
+        setlist: &item.setlist,
+        updated_at: chrono::Local::now().naive_local(),
+        concert_type_id: item.concert_type_id,
+        artist_id: item.artist_id,
+        venue_id: item.venue_id,
+    };   
+    let res = diesel::update(&concert).set(&update).get_result(&conn)?;
+    Ok(res)
+}
+
+// Handler for DELETE /concerts/{id}
+pub async fn delete_concert(db: web::Data<Pool>, concert_id: web::Path<i64>) -> Result<HttpResponse, Error> {
+    Ok(
+        web::block(move || db_delete_single_concert(db, concert_id.into_inner()))
+            .await
+            .map(|concert| HttpResponse::Ok().json(concert))
+            .map_err(|_| HttpResponse::InternalServerError())?,
+    )
+}
+
+fn db_delete_single_concert(db: web::Data<Pool>, concert_id: i64) -> Result<String, diesel::result::Error> {
+    let conn = db.get().unwrap();
+    let count = delete(concerts.find(concert_id)).execute(&conn)?;
+    let message = format!("Total of deletions: {}", count.to_string());
+    Ok(message)
 }
--- src/models.rs
+++ src/models.rs
@@ -104,25 +104,45 @@ pub struct UpdateConcertType<'a> {
 #[derive(Debug, Serialize, Deserialize, Queryable, Identifiable)]
 pub struct Concert {
     pub id : i64,
-    pub concert_date : chrono::NaiveDateTime,
-    pub setlist : String,
+    pub concert_date : chrono::NaiveDate,
+    pub setlist : Option<String>,
+    pub created_at : chrono::NaiveDateTime,
+    pub updated_at : chrono::NaiveDateTime,       
+    pub concert_type_id : i64,
     pub artist_id : i64,
     pub venue_id : i64,
-    pub concert_type_id : i64,
-    pub created_at : chrono::NaiveDateTime,
-    pub updated_at : chrono::NaiveDateTime,
 }
 
 #[derive(Insertable, Debug)]
 #[table_name = "concerts"]
 pub struct NewConcert<'a> {
     pub concert_date: chrono::NaiveDate,
-    pub setlist: &'a str,
+    pub setlist: &'a str,    
+    pub created_at : chrono::NaiveDateTime,
+    pub updated_at : chrono::NaiveDateTime,
+    pub concert_type_id : i64,
+    pub artist_id : i64,
+    pub venue_id : i64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InputConcert {
+    pub concert_date: String,
+    pub setlist: String,
     pub artist_id : i64,
     pub venue_id : i64,
     pub concert_type_id : i64,
-    pub created_at : chrono::NaiveDateTime,
+}
+
+#[derive(AsChangeset)]
+#[table_name="concerts"]
+pub struct UpdateConcert<'a> {
+    pub concert_date: chrono::NaiveDate,
+    pub setlist: &'a str,    
     pub updated_at : chrono::NaiveDateTime,
+    pub concert_type_id : i64,
+    pub artist_id : i64,
+    pub venue_id : i64,    
 }
 
 
